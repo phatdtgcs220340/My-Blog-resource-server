b976f6874662690dd5678c0d0e4bad9e082e2f81
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-08 23:21:23 +0700

    store user in user context post login for better performance

---
+++ src/main/java/com/phatdo/blog/resourceserver/authentication/CustomFilter.java
@@ -0,0 +1,47 @@
+package com.phatdo.blog.resourceserver.authentication;
+
+import com.phatdo.blog.resourceserver.models.User;
+import com.phatdo.blog.resourceserver.services.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CustomFilter extends OncePerRequestFilter {
+    private final UserService userService;
+
+    @Autowired
+    public CustomFilter(UserService userService) {
+        this.userService = userService;
+    }
+
+    /***
+     * An event listener after log in with jwt token
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        JwtAuthenticationToken authentication = (JwtAuthenticationToken) SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+        if (authentication != null) {
+            String fullName = authentication.getTokenAttributes().get("fullName").toString();
+            User user = userService.processOAuth2Login(fullName, authentication.getName(), authentication.getAuthorities());
+            UserContext.setUser(user);
+        }
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            UserContext.clear();
+        }
+    }
+}
---
+++ src/main/java/com/phatdo/blog/resourceserver/authentication/UserContext.java
@@ -0,0 +1,19 @@
+package com.phatdo.blog.resourceserver.authentication;
+
+import com.phatdo.blog.resourceserver.models.User;
+
+public class UserContext {
+    private static final ThreadLocal<User> userHolder = new ThreadLocal<>();
+
+    public static void setUser(User user) {
+        userHolder.set(user);
+    }
+
+    public static User getUser() {
+        return userHolder.get();
+    }
+
+    public static void clear() {
+        userHolder.remove();
+    }
+}
