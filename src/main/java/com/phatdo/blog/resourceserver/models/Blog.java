22fa3d57fc5a28b767ca0e4606a5fa1af0341970
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-08 16:58:45 +0700

    build models

---
+++ src/main/java/com/phatdo/blog/resourceserver/models/Blog.java
@@ -0,0 +1,51 @@
+package com.phatdo.blog.resourceserver.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "\"blog\"")
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+@RequiredArgsConstructor
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false, name = "blog_id")
+    private long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private Timestamp createdDate;
+
+    private Timestamp modifiedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private final User user;
+
+    @OneToMany(mappedBy = "blog",
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private final Set<BlogLike> likes = new HashSet<>();
+
+    @OneToMany(mappedBy = "blog",
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private final Set<Reply> replies = new HashSet<>();
+}
