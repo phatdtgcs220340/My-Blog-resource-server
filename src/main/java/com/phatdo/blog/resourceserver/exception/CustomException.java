2b016404f1dc2f7669193fa3de23fc9ad5460517
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-09 13:11:04 +0700

    add interface

--- src/main/java/com/phatdo/blog/resourceserver/exception/CustomException.java
+++ src/main/java/com/phatdo/blog/resourceserver/exception/CustomException.java
@@ -1,13 +1,19 @@
 package com.phatdo.blog.resourceserver.exception;
 
+import com.phatdo.blog.resourceserver.classification.TypeDTO;
 import lombok.Getter;
 
 @Getter
-public class CustomException extends Exception{
+public class CustomException extends Exception implements TypeDTO {
     private final int code;
 
     public CustomException(CustomError error) {
         super(error.getMessage());
         this.code = error.getCode();
+    }
+
+    @Override
+    public String type() {
+        return "error-dto";
     }
 }
