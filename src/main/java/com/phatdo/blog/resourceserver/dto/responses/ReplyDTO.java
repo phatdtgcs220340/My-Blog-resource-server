13bae7936b8e1cc5ab62dad640c1a4472277ea6c
tuPC <phatdtgcs220340@fpt.edu.vn>
2024-06-10 11:12:29 +0700

    reply dto

---
+++ src/main/java/com/phatdo/blog/resourceserver/dto/responses/ReplyDTO.java
@@ -0,0 +1,17 @@
+package com.phatdo.blog.resourceserver.dto.responses;
+
+import com.phatdo.blog.resourceserver.classification.TypeDTO;
+
+public record ReplyDTO(
+        long id,
+        String content,
+        String updatedDate,
+        long userId,
+        String username,
+        int totalLikes
+) implements TypeDTO {
+    @Override
+    public String type() {
+        return "reply-dto";
+    }
+}
