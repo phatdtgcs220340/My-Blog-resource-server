369863801cc481a799423cb89dd75ba77d512206
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-21 00:20:49 +0700

    remove unnecessary stuffs

--- src/main/java/com/phatdo/blog/resourceserver/controllers/BlogController.java
+++ src/main/java/com/phatdo/blog/resourceserver/controllers/BlogController.java
@@ -4,17 +4,16 @@ import com.phatdo.blog.resourceserver.authentication.UserContext;
 import com.phatdo.blog.resourceserver.classification.TypeDTO;
 import com.phatdo.blog.resourceserver.dto.requests.CreateBlogDTO;
 import com.phatdo.blog.resourceserver.dto.requests.UpdateBlogDTO;
-import com.phatdo.blog.resourceserver.exception.CustomError;
 import com.phatdo.blog.resourceserver.exception.CustomException;
 import com.phatdo.blog.resourceserver.models.Blog;
 import com.phatdo.blog.resourceserver.models.User;
 import com.phatdo.blog.resourceserver.services.BlogService;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,15 +28,10 @@ public class BlogController {
     }
 
     @PostMapping
-    public ResponseEntity<TypeDTO> createBlog(@RequestBody CreateBlogDTO form) {
-        try {
-            User user = UserContext.getUser();
-            Blog blog = blogService.saveBlog(form.title(), form.content(), form.type(), user);
-            return ResponseEntity.ok(blog.toDTO());
-        }
-        catch (Exception e) {
-            return new ResponseEntity<>((new CustomException(CustomError.INVALID_FORM)).toDTO(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<TypeDTO> createBlog(@RequestBody @Valid CreateBlogDTO form) {
+        User user = UserContext.getUser();
+        Blog blog = blogService.saveBlog(form.title(), form.content(), form.type(), user);
+        return ResponseEntity.ok(blog.toDTO());
     }
 
     @GetMapping("/{id}")
@@ -53,20 +47,19 @@ public class BlogController {
     public ResponseEntity<Page<TypeDTO>> getBlogs(@RequestParam(defaultValue = "0") int page,
                                                   @RequestParam(defaultValue = "5") int size) {
         Pageable pageable = PageRequest.of(page, size);
-        return ResponseEntity.ok(blogService.getAllBlogs(pageable).map(Blog::toDTO));
+        return ResponseEntity.ok(blogService.getAllBlogs(pageable).map(Blog::toPartialDTO));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<TypeDTO> updateBlog(@RequestBody UpdateBlogDTO form, @PathVariable long id) {
+    public ResponseEntity<TypeDTO> updateBlog(@RequestBody UpdateBlogDTO form,
+                                              @PathVariable long id) {
         try {
-            return ResponseEntity.ok(blogService
-                    .updateBlog(
+            return ResponseEntity.ok(blogService.updateBlog(
                             id,
                             form.title(),
-                            form.content())
-                    .toDTO());
-        } catch (Exception e) {
-            return new ResponseEntity<>((new CustomException(CustomError.INVALID_FORM)).toDTO(), HttpStatus.BAD_REQUEST);
+                            form.content()).toDTO());
+        } catch (CustomException e) {
+            return new ResponseEntity<>(e.toDTO(), e.getStatus());
         }
     }
 
