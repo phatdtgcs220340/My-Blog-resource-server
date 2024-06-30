1ecb2ca3228a4f1a8bcd739d261d6a5d70d35b78
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-30 21:29:15 +0700

    add map

---
+++ src/main/java/com/phatdo/blog/resourceserver/mappers/DTOMapperE.java
@@ -0,0 +1,8 @@
+package com.phatdo.blog.resourceserver.mappers;
+
+public enum DTOMapperE {
+    BLOG,
+    USER,
+    REPLY,
+    ERROR
+}
--- src/main/java/com/phatdo/blog/resourceserver/mappers/DTOMapperFactory.java
+++ src/main/java/com/phatdo/blog/resourceserver/mappers/DTOMapperFactory.java
@@ -1,17 +1,27 @@
 package com.phatdo.blog.resourceserver.mappers;
 
+import com.phatdo.blog.resourceserver.mappers.suppliers.BlogMapper;
+import com.phatdo.blog.resourceserver.mappers.suppliers.ErrorMapper;
+import com.phatdo.blog.resourceserver.mappers.suppliers.ReplyMapper;
+import com.phatdo.blog.resourceserver.mappers.suppliers.UserMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class DTOMapperFactory {
+    private final Map<DTOMapperE, DTOMapper<?>> mappers = Map.of(
+            DTOMapperE.USER, new UserMapper(),
+            DTOMapperE.BLOG, new BlogMapper(),
+            DTOMapperE.REPLY, new ReplyMapper(),
+            DTOMapperE.ERROR, new ErrorMapper()
+    );
+
     @SuppressWarnings("unchecked")
-    public <T> DTOMapper<T> getMapper(String model) {
-        return (DTOMapper<T>) switch (model) {
-            case "user" -> new UserMapper();
-            case "blog" -> new BlogMapper();
-            case "reply" -> new ReplyMapper();
-            case "error" -> new ErrorMapper();
-            default -> throw new IllegalArgumentException("Unknown model: " + model);
-        };
+    public <T> DTOMapper<T> getMapper(DTOMapperE type) {
+        if (mappers.containsKey(type)) {
+            return (DTOMapper<T>) mappers.get(type);
+        }
+        else throw new IllegalArgumentException("Invalid model: " + type);
     }
 }
