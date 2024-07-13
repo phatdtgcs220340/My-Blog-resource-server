18d52e5097b4a486a68992ea7f70353cb869b361
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-07-14 00:13:30 +0700

    enable async

--- src/main/java/com/phatdo/blog/resourceserver/ResourceServerApplication.java
+++ src/main/java/com/phatdo/blog/resourceserver/ResourceServerApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableAspectJAutoProxy
+@EnableAsync
 public class ResourceServerApplication {
 
     public static void main(String[] args) {
