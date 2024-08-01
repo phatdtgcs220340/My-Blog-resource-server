4e4d6186beb1517ccf782c71f377a6d4e865d5d7
lãng tử cam ranh <114555261+phatdtgcs220340@users.noreply.github.com>
2024-08-02 01:23:42 +0700

    utilize id token

--- src/main/java/com/phatdo/blog/resourceserver/controllers/UserController.java
+++ src/main/java/com/phatdo/blog/resourceserver/controllers/UserController.java
@@ -4,24 +4,33 @@ import com.phatdo.blog.resourceserver.authentication.UserContext;
 import com.phatdo.blog.resourceserver.dto.responses.TypeDTO;
 import com.phatdo.blog.resourceserver.mappers.DTOMapperE;
 import com.phatdo.blog.resourceserver.mappers.DTOMapperFactory;
+import com.phatdo.blog.resourceserver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping(path = "/api/v1/user", produces = "application/json")
 public class UserController {
     private final DTOMapperFactory mapperFactory;
+    private final UserService userService;
 
     @Autowired
-    public UserController(DTOMapperFactory mapperFactory) {
+    public UserController(DTOMapperFactory mapperFactory, UserService userService) {
         this.mapperFactory = mapperFactory;
+        this.userService = userService;
     }
 
     @GetMapping
     public ResponseEntity<TypeDTO> getUser() {
         return ResponseEntity.ok(mapperFactory.getMapper(DTOMapperE.USER).toDTO(UserContext.getUser()));
+    }
+
+    @PostMapping
+    public ResponseEntity<TypeDTO> createUser(@RequestHeader("Authorization") String token) throws ParseException {
+        userService.register(token.replace("Bearer ", ""));
+        return ResponseEntity.ok().build();
     }
 }
