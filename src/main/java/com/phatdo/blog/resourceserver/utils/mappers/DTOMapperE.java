9e939728e209d0bcf6bab38595b0a74407b17ae9
Phat Do <114555261+phatdtgcs220340@users.noreply.github.com>
2024-09-17 04:42:13 +0700

    update mapper

--- src/main/java/com/phatdo/blog/resourceserver/utils/mappers/DTOMapperE.java
+++ src/main/java/com/phatdo/blog/resourceserver/utils/mappers/DTOMapperE.java
@@ -1,11 +1,22 @@
 package com.phatdo.blog.resourceserver.utils.mappers;
 
+import com.phatdo.blog.resourceserver.utils.commons.dto.TypeDTO;
+import com.phatdo.blog.resourceserver.utils.mappers.suppliers.*;
+import lombok.Getter;
+
+@Getter
 public enum DTOMapperE {
-    BLOG,
-    USER,
-    REPLY,
-    PARTIAL_BLOG,
-    ERROR,
-    TAG,
-    FILE_DETAIL
+    BLOG(new BlogMapper()),
+    USER(new UserMapper()),
+    REPLY(new ReplyMapper()),
+    PARTIAL_BLOG(new PartialBlogMapper()),
+    ERROR(new ErrorMapper()),
+    TAG(new TagMapper()),
+    FILE_DETAIL(new FileDetailMapper());
+
+    private final DTOMapper<?, ? extends TypeDTO> mapper;
+
+    DTOMapperE(DTOMapper<?, ? extends TypeDTO> mapper) {
+        this.mapper = mapper;
+    }
 }
--- src/main/java/com/phatdo/blog/resourceserver/utils/mappers/DTOMapperFactory.java
+++ src/main/java/com/phatdo/blog/resourceserver/utils/mappers/DTOMapperFactory.java
@@ -8,21 +8,8 @@ import java.util.Map;
 
 @Component
 public class DTOMapperFactory {
-    private final Map<DTOMapperE, DTOMapper<?, ? extends TypeDTO>> mappers = Map.of(
-            DTOMapperE.USER, new UserMapper(),
-            DTOMapperE.BLOG, new BlogMapper(),
-            DTOMapperE.REPLY, new ReplyMapper(),
-            DTOMapperE.PARTIAL_BLOG, new PartialBlogMapper(),
-            DTOMapperE.ERROR, new ErrorMapper(),
-            DTOMapperE.TAG, new TagMapper(),
-            DTOMapperE.FILE_DETAIL, new FileDetailMapper()
-    );
-
     @SuppressWarnings("unchecked")
     public <T> DTOMapper<T, TypeDTO> getMapper(DTOMapperE type) {
-        if (mappers.containsKey(type)) {
-            return (DTOMapper<T, TypeDTO>) mappers.get(type);
-        }
-        else throw new IllegalArgumentException("Invalid model: " + type);
+       return (DTOMapper<T, TypeDTO>) type.getMapper();
     }
 }
